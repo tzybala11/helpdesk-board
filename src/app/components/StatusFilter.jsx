d95e3be07e8dd84334254457eb9dec38d002bb84
Tristan Zybala <tzybala@usf.edu>
2025-11-09 21:02:42 -0500

    Moved contents of StatusFilter to StatusMessage. Did not notice I was in the wrong file earlier until I just realized.

--- src/app/components/StatusFilter.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export default function StatusMessage({ loading, error, isEmpty }) {
-    if (loading) return <div className=''>Loading, please wait...</div>;
-    if (error) return <div className=''>Unable to load page. Please try again later.</div>;
-    if (isEmpty) return <div className=''>No tickets to display matching your filters.</div>;
-    return null;
-} //INPUT CSS FOR EACH DIV STATEMENT
