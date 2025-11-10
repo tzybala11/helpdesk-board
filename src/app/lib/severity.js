b25a79dc21731ce69543508656058886d74e36d8
Tristan Zybala <tzybala@usf.edu>
2025-11-09 20:47:45 -0500

    Created Severity JS file and updated folder & file location.

---
+++ src/app/lib/severity.js
@@ -0,0 +1,13 @@
+export const priorityOrder = {
+    Low: 1,
+    Medium: 2,
+    High: 3,
+    Critical: 4
+};
+
+export const statusOrder = {
+    'Open': 1,
+    'In Progress': 2,
+    'On Hold': 3,
+    'Resolved': 4
+};
